d3d12da0fd0b37eca518b527aa39dbac197ef3d6
Michele Curtaz <mcurtaz@gmail.com>
2020-07-07 11:51:56 +0200

    Parte grafica Jumbotron

--- js/script.js
+++ js/script.js
@@ -8,4 +8,14 @@ $(document).ready(function(){
   $(".links-wrapper .main-dropdown").mouseleave(function(){ // mouse esce dal li con classe main-dropdown e do funzione hide ai figli con classe main-dropdown-content
     $(this).children(".main-dropdown-content").hide();
   });
+
+  // piccolo esperimento. quando c'è il focus sull'input numero di telefono mostro la label corrispondente. .focus() e .focusout() funzionano tipo mauseenter e mouseleave.
+  $("input[name=\"numerocel\"]").focus(function(){
+    $("label[for=\"numerocel\"]").fadeIn();
+  });
+
+  $("input[name=\"numerocel\"]").focusout(function(){
+    $("label[for=\"numerocel\"]").fadeOut();
+  });
+
 });
